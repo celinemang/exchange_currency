45b4ac2e48f5a3e3fce4bea3e4d56d390943ce0f
Celine Mang <53549166+celinemang@users.noreply.github.com>
2024-02-10 06:36:22 +0900

    Update main.js

--- main.js
+++ main.js
@@ -7,67 +7,94 @@
 //6. 숫자를 한국어로 읽는법 -> 영어로
 //7. 반대로 밑에 박스에서 숫자를 바꿔도 위에 박스에 환율이 적용된다.
 
+const currencyRatio = {
+  VND: {
+    USD: 0.000043,
+    KRW: 0.051,
+    VND: 1,
+    unit: "동",
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Flag_of_Vietnam.svg/2560px-Flag_of_Vietnam.svg.png",
+  },
+  USD: {
+    USD: 1,
+    KRW: 1182.35,
+    VND: 23235.5,
+    unit: "달러",
+    img: "https://cdn-icons-png.flaticon.com/512/555/555526.png",
+  },
+  KRW: {
+    USD: 0.00085,
+    KRW: 1,
+    VND: 19.47,
+    unit: "원",
+    img: "https://cdn.countryflags.com/thumbs/south-korea/flag-400.png",
+  },
+};
+var unitWords = ["", "만", "억", "조", "경"];
+var splitUnit = 10000;
+let toButton = document.getElementById("to-button");
+let fromButton = document.getElementById("from-button");
+let toCurrency = "USD";
+let fromCurrency = "USD";
 
-let currencyRatio = { //변수
-    USD:{
-        KRW:1336.95,
-        USD: 1,
-        VND:24380.00,
-        unit:"dollar"
-    },
-    KRW:{
-        KRW:1,
-        USD:0.00075,
-        VND:18.24,
-        unit:"won"
-    },
-    VND:{
-        VND:1,
-        KRW:0.055,
-        USD:0.000041,
-        unit:"đồng"
-    }
+document.querySelectorAll("#from-currency-list li").forEach(function (item) {
+  item.addEventListener("click", function () {
+    fromCurrency = this.id;
+    fromButton.innerHTML = `<img class="flag-img"src="${currencyRatio[fromCurrency].img}"/>${fromCurrency}`;
+    convert("from");
+  });
+});
 
+document.querySelectorAll("#to-currency-list li").forEach(function (item) {
+  item.addEventListener("click", function () {
+    toCurrency = this.id;
+    toButton.innerHTML = `<img class="flag-img"src="${currencyRatio[toCurrency].img}"/>${toCurrency}`;
+    convert("from");
+  });
+});
+
+function convert(type) {
+  console.log("here");
+  let amount = 0;
+  if (type == "from") {
+    //입력갑 받기
+    amount = document.getElementById("fromAmount").value;
+    // 환전하기
+    let convertedAmount = amount * currencyRatio[fromCurrency][toCurrency];
+    // 환전한값 보여주기
+    document.getElementById("toAmount").value = convertedAmount;
+    //환전한값 한국어로
+    renderKoreanNumber(amount, convertedAmount);
+  } else {
+    amount = document.getElementById("toAmount").value;
+    let convertedAmount = amount * currencyRatio[toCurrency][fromCurrency];
+    document.getElementById("fromAmount").value = convertedAmount;
+    renderKoreanNumber(convertedAmount, amount);
+  }
 }
-
-//1. console.log(currencyRatio.VND.unit);
-
-//console.log(currencyRatio["VND"]["unit"]);
-
-let fromCurrency = 'USD';
-
-document
-.querySelectorAll("#from-currency-list a").forEach((menu) => 
-    menu.addEventListener("click",function (){
-    //1.버튼으ㅡㄹ 가벼온다
-    document.getElementById("from-button").textContent = this.textContent;
-
-    //2. 버튼의 값을 바꾼다.
-    //.textContent = this.textContent;
-
-    //3. 선택된 currency 값을 변수에 저장해둔다.
-        fromCurrency = this.textContent;
-        console.log("fromcurrency는",fromCurrency);
-
-})
-);
-
-
-document
-.querySelectorAll("#to-currency-list a").forEach((menu) => 
-    menu.addEventListener("click",function (){
-    //1.버튼으ㅡㄹ 가벼온다
-    document.getElementById("to-button").textContent = this.textContent;
-
-    //2. 버튼의 값을 바꾼다.
-    //.textContent = this.textContent;
-
-    //3. 선택된 currency 값을 변수에 저장해둔다.
-        toCurrency = this.textContent;
-        console.log("tocurrency는",fromCurrency);
-
-})
-);
+function renderKoreanNumber(from, to) {
+  document.getElementById("fromNumToKorea").textContent =
+    readNum(from) + currencyRatio[fromCurrency].unit;
+  document.getElementById("toNumToKorea").textContent =
+    readNum(to) + currencyRatio[toCurrency].unit;
+}
+function readNum(num) {
+  let resultString = "";
+  let resultArray = [];
+  for (let i = 0; i < unitWords.length; i++) {
+    let unitResult =
+      (num % Math.pow(splitUnit, i + 1)) / Math.pow(splitUnit, i);
+    unitResult = Math.floor(unitResult);
+    if (unitResult > 0) {
+      resultArray[i] = unitResult;
+    }
+  }
+  for (let i = 0; i < resultArray.length; i++) {
+    if (!resultArray[i]) continue;
+    resultString = String(resultArray[i]) + unitWords[i] + resultString;
+  }
+  return resultString;
+}
 
 
 //1. 키를 입력하는 순간
